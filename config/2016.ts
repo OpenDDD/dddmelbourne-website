8e3fbf9ec48f03ded279e8cc057bd28820f6dde3
Tien Phan <skynetx@gmail.com>
2019-04-13 19:55:03 +1000

    Create 2016.ts

---
+++ config/2016.ts
@@ -0,0 +1,18 @@
+import { Sponsor, SponsorType } from './types'
+
+interface From2016 {
+  YouTubePlaylistUrl: string
+  YouTubeKeynoteEmbedUrl: string
+  FlickrAlbumUrl: string
+  Sponsors: Sponsor[]
+}
+
+const From2016: From2016 = {
+  YouTubePlaylistUrl: null, // TODO
+  // tslint:disable-next-line:object-literal-sort-keys
+  YouTubeKeynoteEmbedUrl: null, // TODO
+  FlickrAlbumUrl: null, // TODO
+  Sponsors: []
+}
+
+export default From2016
