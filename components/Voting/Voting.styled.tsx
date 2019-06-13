8e85d11b90fc71bd9a9ebf54a84ee26069fee8ba
Vatsalya Goel <vatsalya@goel.net.in>
2019-06-14 09:48:14 +1000

    React overlays

---
+++ components/Voting/Voting.styled.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+import { zIndex } from '../utils/styles/zindex'
+
+export const StyledVotingPanel = styled.div`
+ position: sticky;
+ top: 0;
+ z-index: ${zIndex.votingPanel};
