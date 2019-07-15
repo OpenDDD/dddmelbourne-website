6334e58b5fb1857da2c4ef98d527187329124594
Vatsalya Goel <vatsalya@goel.net.in>
2019-07-15 18:55:32 +1000

    fix lint:

--- components/utils/styles/zindex.ts
+++ components/utils/styles/zindex.ts
@@ -1,7 +1,6 @@
-// tslint:disable-next-line: object-literal-sort-keys
-
 export const zIndex = {
   votingPanel: 200,
+  // tslint:disable-next-line: object-literal-sort-keys
   testingControl: 100,
   agendaOverlay: 20,
   agendaItem: 10,
